7f48586ff5416d1767983f79eae5608f7ee7dac2
Nadia Salem <nsalemprofessional@gmail.com>
2026-04-03 17:57:08 -0700

    Basic Ceremony look

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(700, 600);
+}
+
+function draw() {
+  background(235,240,245);
+  fill(40, 70, 120);
+  rect(0, 40, 700, 50);
+  fill(160, 120, 70)
+  rect(0, 400, 700, 200);
+  
+}
