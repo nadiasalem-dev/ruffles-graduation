cb230aeebb603a4946707817fabab29dd35b9c00
Nadia Salem <nsalemprofessional@gmail.com>
2026-04-04 15:49:13 -0700

    Update sketch.js
    Create array of 18 Cat objects and add podium movement with pause logic

--- sketch.js
+++ sketch.js
@@ -1,12 +1,67 @@
+let timeFinish = false;
+let rufflesX = 20;
+let catCurrent = 0;
+let timer = 0;
+let cat = []; 
 function setup() {
   createCanvas(700, 600);
+  for(let i= 0; i < 18; i++)
+    cat[i] = new Cat(10, 580, color(155,255,255));
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(235,240,245);
+  //Banner
   fill(40, 70, 120);
-  rect(0, 40, 700, 50);
+  rect(0, 0, 700, 100);
+  //Words on Banner
+  textSize(30);
+  textAlign(CENTER);
+  fill(255)
+  text("Catty University, Naptime", 350, 40);
+  textSize(29);
+  text("Graduation 2026", 350, 80);
+  //Stage
+  noStroke();
   fill(160, 120, 70)
   rect(0, 400, 700, 200);
+  noStroke();
+  // Podium
+  fill(40,70, 120);
+  rect(300, 525, 100, 40);
+  //Cats Movement
+  if(catCurrent < cat.length){
+    if(!cat[catCurrent].atPodium()){
+      cat[catCurrent].move();
+       
+    }
+    else if(timeFinish === false ){
+      timer++;
+       if(catCurrent + 1 < cat.length && !cat[catCurrent +1].atPodium())
+          cat[catCurrent + 1].move();  
+
+      if(timer === 100){
+        timeFinish = true;
+      }
+    }
+      else{
+        cat[catCurrent].move();
+        if(catCurrent + 1 < cat.length && !cat[catCurrent + 1].atPodium()){
+  cat[catCurrent + 1].move();
+}
+      }
+      if(cat[catCurrent].offScreen()){
+        catCurrent++;
+        timer = 0;
+        timeFinish = false;
+      }
+    
   
+  }
+  //Cats display
+  for(let i = 0; i <  cat.length; i++)   {
+    cat[i].display();
+    
+  }
 }
