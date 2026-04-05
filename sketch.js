8944d725a89414180caa438df5511365b748b464
Nadia Salem <nsalemprofessional@gmail.com>
2026-04-05 16:14:56 -0700

    Update sketch.js
    update to scale the animation for all browsers

--- sketch.js
+++ sketch.js
@@ -4,35 +4,42 @@ let catCurrent = 0;
 let timer = 0;
 let cat = [];
 let compie;
+let sx;
+let sy;
 function setup() {
-  createCanvas(700, 600);
+createCanvas(windowWidth, windowWidth * (6 / 7));
   for (let i = 0; i < 18; i++) cat[i] = new Cat(10, 580, color(155, 255, 255));
   angleMode(DEGREES);
+  sx = width/700;
+  sy = height/600;
 }
 function preload() {
   compie = loadImage("compie.png");
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowWidth * (6 / 7));
 }
 function draw() {
   background(235, 240, 245);
   //Banner
   fill(40, 70, 120);
-  rect(0, 0, 700, 100);
+  rect(0, 0, sx* 700, sy *100);
   //Words on Banner
   textSize(30);
   textAlign(CENTER);
   fill(255);
-  text("Catty University, Naptime", 350, 40);
+  text("Catty University, Naptime", sx * 350, sy * 40);
   textSize(29);
-  text("Graduation 2026", 350, 80);
+  text("Graduation 2026", sx * 350, sy * 80);
   //Stage
   noStroke();
   fill(160, 120, 70);
-  rect(0, 400, 700, 200);
+  rect(0, sx * 400, sy * 700, sy * 200);
   //Display laptop computer as presenter
-  image(compie, 270, 440, 160, 130);
+  image(compie, sx * 270, sx * 440, sy * 160, sy * 150);
   // Podium
   fill(40, 70, 120);
-  rect(300, 525, 100, 40);
+  rect(sx * 300, sx * 525, sy * 100, sy * 40);
   //Cats Movement
   if (catCurrent < cat.length) {
     if (!cat[catCurrent].atPodium()) {
@@ -63,9 +70,9 @@ function draw() {
   }
   //Coffee Cup
   fill(255);
-  rect(370, 540, 10, 20);
+  rect(sx * 370, sx * 540, sy * 10, sy * 20);
   noFill();
   stroke(255);
 
-  arc(384, 550, 10, 10, 45, 30);
+  arc(sx * 384, sy *  550, 10, 10, 45, 30);
 }
