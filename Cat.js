8beeea8a95d89d27edc3d73d581dde6c2d280d0f
Nadia Salem <nsalemprofessional@gmail.com>
2026-04-26 11:49:31 -0700

    Update Cat.js
    Still fixing the shadow

--- Cat.js
+++ Cat.js
@@ -33,7 +33,7 @@ class Cat {
       frame.sx,
       frame.sy,
       frame.sw,
-      frame.sh
+      frame.sh -40
     );
     this.awardType = "";
     if (this.spriteIndex === 17) this.awardType = this.awardException;
